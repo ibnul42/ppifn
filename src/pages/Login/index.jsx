dda2fef644f61dd7957faffe148b5d64c970951d
Ibnul Ashir <ibnulashir42@gmail.com>
2022-05-24 13:11:39 +0600

    all types of footer

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,31 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { Dropdown } from "react-bootstrap";
+import { FcGoogle } from "react-icons/fc";
 
 function Login() {
+  const [loginType, setLoginType] = useState(null);
+  const [TypeErrorTxt, setTypeErrorTxt] = useState(false);
+  const submitHandler = (e) => {
+    setTypeErrorTxt(true);
+    e.preventDefault();
+    console.log("Submit");
+  };
+
+  const typeChanged = (input) => {};
   return (
     <div className="row">
       <div className="col-sm-12 col-md-6">
         <p className="h2">Account Login</p>
         <div className="col-12 col-md-9">
-          <form action="" className=" d-flex flex-column my-4 gap-3">
-            <input
-              type="text"
-              placeholder="email or username"
-              className="py-1 px-2 border"
-            />
-            <input type="password" className="py-1 px-2 border" />
+          <form
+            onClick={submitHandler}
+            className=" d-flex flex-column my-4 gap-3"
+          >
+            <div className="">
+              <label htmlFor="dropdown-type" className="my-2">
+                Please select a type
+              </label>
+              <Dropdown>
+                <Dropdown.Toggle variant="warning" id="dropdown-type">
+                  {loginType ? loginType : "None"}
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={() => setLoginType("Pool")}>
+                    Pool
+                  </Dropdown.Item>
+                  <Dropdown.Item onClick={() => setLoginType("Participant")}>
+                    Participants
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              {TypeErrorTxt ? "Type required" : null}
+            </div>
+            <div className="w-100">
+              <p>Email</p>
+              <input
+                type="text"
+                placeholder="email or username"
+                className="py-1 px-2 border w-100 w-100"
+              />
+            </div>
+            <div className="w-100">
+              <p>Password</p>
+              <input type="password" className="py-1 px-2 border w-100" />
+            </div>
+
             <input
               type="submit"
               name="login"
               value="Sign In"
-              className="py-1 px-2 border"
+              className="py-2 px-2 border my-2"
             />
           </form>
         </div>
         <div className="col-12 col-md-9 d-flex justify-content-center">
           <p>Or sign in with</p>
         </div>
-        <div className="col-12 col-md-9 d-flex ">
-          <div className="col-6 d-flex justify-content-start">
+        <div className="col-12 col-md-9 d-flex position-relative">
+          <div className="col-6 d-flex justify-content-end">
             <input
               type="button"
               name="google"
@@ -51,7 +92,9 @@ function Login() {
           </p>
         </div>
       </div>
-      <div className="col-sm-12 col-md-6">'dxm</div>
+      <div className="col-sm-12 col-md-6">
+        <img src="/assets/login.svg" className="h-100 w-100" alt="" />
+      </div>
     </div>
   );
 }
